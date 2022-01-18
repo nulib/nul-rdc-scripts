892f1c22ac4ea75645f24ae22e55df3ddc71cfe7
Joshua Yocum <joshua.yocum@northwestern.edu>
2022-01-18 11:23:06 -0600

    Added run script

---
+++ Meadow/mig_av/run.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import sys
+if sys.version_info[0] < 3:
+    raise Exception("Python 3 or a more recent version is required.")
+
+def main():
+    import mig_av.meadow_ingest_generator_av as mig_av
+    mig_av.mig_av_main()
+
+if __name__ == "__main__":
+    main()
