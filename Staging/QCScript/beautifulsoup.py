314b2bc2bf5705764a5af503f38d5408683f1ec7
SophiaFrancis1 <112432333+SophiaFrancis1@users.noreply.github.com>
2023-10-11 08:49:53 -0500

    Testing

--- Staging/QCScript/beautifulsoup.py
+++ Staging/QCScript/beautifulsoup.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from bs4 import BeautifulSoup 
-
+import matplotlib as mpl
 
 file = open("/Users/dcstaff/Documents/GitHub/nul-rdc-scripts/Staging/QCScript/vlc-record-2023-10-05-11h16m25s-test_NONEVIDEOSETTING_QTUNCOMPRESSED-p - Copy.mkv-.avi.qctools.xml")
 contents = file.read()
@@ -8,14 +8,41 @@ contents = file.read()
 soup = BeautifulSoup(contents, 'xml')
 framevalues = {}
 framedata = {}
+framedataonly = {}
+tagkeylist = []
 
 for frames in soup.find_all('frame'):
     frametime = frames.get('pkt_pts_time')
     taglist = frames.find_all('tag')
-
+    framedata['frametime'] = frametime
     for tag in taglist:
         tagkey = tag.get('key')
-        tagvalue = tag.get('value')
-        framedata['frametime'] = frametime
-        framedata[tagkey] = tagvalue
+        tagvalue = tag.get(str('value'))
+        datapoint = tagkey + ':' + tagvalue
+        framedataonly[tagkey] = tagvalue
+        tagkeylist=tagkey
+        framedata['datapoint']=datapoint
+        framedata[frametime]=framedataonly
+        df = pd.DataFrame(framedata, columns=[tagkeylist])
+        df
     
+    
+'''
+    for tag in taglist:
+        tagkey = tag.get('key')
+        for key in tagkey:
+            tagvalue = tag.get(str('value'))
+            df = pd.DataFrame
+        tagkeylist.append(tagkey)
+        framedata[tagkey] = tagvalue
+        df = pd.DataFrame(framedata)
+
+        df['FrameDataMatrix'] = df['']
+'''
+
+
+'''
+    df = pd.DataFrame(framedata, columns = tagkeylist, index = [0])
+    df['FrameData'] = df['']
+    print(tagkeylist)
+'''
