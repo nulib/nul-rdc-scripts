f9c8903ff0a8709338b0722638ae7b05fadac995
SophiaFrancis1 <112432333+SophiaFrancis1@users.noreply.github.com>
2023-10-11 08:49:53 -0500

    Testing

--- Staging/QCScript/try.py
+++ Staging/QCScript/try.py
@@ -1,5 +1,29 @@
-import xmlschema
-import json
+import pandas as pd
+from bs4 import BeautifulSoup 
+import matplotlib as mpl
 
-xml_document = '/Users/dcstaff/Documents/GitHub/nul-rdc-scripts/Staging/QCScript/vlc-record-2023-10-05-11h16m25s-test_NONEVIDEOSETTING_QTUNCOMPRESSED-p - Copy.mkv-.avi.qctools.xml'
-print(json.dumps(xmlschema.to_dict(xml_document),indent=4))
+file = open("/Users/dcstaff/Documents/GitHub/nul-rdc-scripts/Staging/QCScript/vlc-record-2023-10-05-11h16m25s-test_NONEVIDEOSETTING_QTUNCOMPRESSED-p - Copy.mkv-.avi.qctools.xml")
+contents = file.read()
+
+soup = BeautifulSoup(contents, 'xml')
+framevalues = {}
+framedata = {}
+framedataonly = {}
+tagkeylist = []
+
+for frames in soup.find_all('frame'):
+    frametime = frames.get('pkt_pts_time')
+    taglist = frames.find_all('tag')
+    framedata['frametime'] = frametime
+    for tag in taglist:
+        tagkey = tag.get('key')
+        tagvalue = tag.get(str('value'))
+        datapoint = tagkey + ':' + tagvalue
+        framedataonly[tagkey] = tagvalue
+        tagkeylist=tagkey
+        framedata['datapoint']=datapoint
+        framedata[frametime]=framedataonly
+        df = pd.DataFrame(framedata, columns=[tagkeylist])
+        df
+    
+    
