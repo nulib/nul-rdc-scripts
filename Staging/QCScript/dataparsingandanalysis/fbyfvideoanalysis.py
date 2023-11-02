89046deb89fe05f20c93e0a80de579bac899e69d
SophiaFrancis1 <112432333+SophiaFrancis1@users.noreply.github.com>
2023-11-02 14:50:18 -0500

    Analysis Additions

--- Staging/QCScript/dataparsingandanalysis/fbyfvideoanalysis.py
+++ Staging/QCScript/dataparsingandanalysis/fbyfvideoanalysis.py
@@ -5,31 +5,75 @@ from data.videovalues8Bit import eightBitVideoValues as eightBitVideoValues
 satI = 0
 satB = 0
 suffix = ["MIN", "MAX", "HIGH", "LOW", "AVG"]
-def checkerrors (videodata, videobitdepth):
-    def setStandardValues ():
+def checkerrors (videodata, videobitdepth, eightBitVideoValues, tenBitVideoValues):
+
+    def setStandardValues (eightBitVideoValues, tenBitVideoValues):
         if videobitdepth == "--8bit" or videobitdepth == "-8":
             standardvalues = eightBitVideoValues
         elif videobitdepth == "--10bit" or videobitdepth == "-10":
             standardvalues = tenBitVideoValues
         return standardvalues
     
-    standardvalues = setStandardValues(videobitdepth)
     
+    def checkLuma (videodata, videobitdepth, standardvalues):
+         criteria = "Y"
+         checkLOWs(videobitdepth, videodata, standardvalues, criteria)
+    
+    def checkChromaU (videodata, videobitdepth, standardvalues):
+         criteria = "U"
+         checkLOWs(videobitdepth, videodata, standardvalues, criteria)
+          
+    def checkChromaV (videodata, videobitdepth, standardvalues):
+         criteria = "V"
+         checkLOWs(videobitdepth, videodata, standardvalues, criteria)
     
     def checkLOWs (videodata, standardvalues, criteria):
             criteria = criteria + "LOW"
-            criteriaBRNG = standardvalues.get(tenBitVideoValues[criteria]["BRNGOut"])
-            criteriaClipping = standardvalues.get(tenBitVideoValues[criteria]["clipping"])
-            criteriaEquation = "" + criteria + "=<" + criteriaBRNG + ""
-            lowErrors = {}
-            dfLowErrors = videodata.query(criteriaEquation)
-            if dfLowErrors.empty:
-                 pass
+            criteriaBRNG = standardvalues.get(criteria) 
+            criteriaClipping = standardvalues.get(criteria)
+            selectColumns = "" + criteria + "," + "Frame Time" + ""
+            lowBRNGErrors = setAndRunLowBRNG(criteriaBRNG, criteria, selectColumns, videodata)
+            lowClippingErrors = setAndRunLowClipping(criteriaClipping, criteria, selectColumns, videodata)
+
+            if lowBRNGErrors.isEmpty and lowClippingErrors.isEmpty:
+                  pass
+            elif lowBRNGErrors.isEmpty or lowClippingErrors.isEmpty:
+                  if lowBRNGErrors.isEmpty:
+                        lowErrors = lowClippingErrors
+                  else:
+                        lowErrors = lowBRNGErrors
             else:
-                 lowErrors["LOW Errors"] = dfLowErrors
-                
+                  lowErrors = [lowClippingErrors,lowBRNGErrors]
             return lowErrors
+                  
+    def setAndRunLowClipping(criteria, criteriaClipping, selectColumns, videodata):        
+        criteriaEquation = "" + criteria + "==" + criteriaClipping + ""
+        lowClippingErrors = {}
+        subDF = videodata[[selectColumns]]
+
+        lowClippingErrors = subDF.query(criteriaEquation)
+        if subDF.empty:
+                 pass
+        else:
+                 lowClippingErrors.assign(errortype="Low Clipping")
                 
+        return lowClippingErrors
+    
+    def setAndRunLowBRNG(criteria, criteriaBRNG, selectColumns, videodata):        
+        criteriaEquation = "" + criteria + "=<" + criteriaBRNG + "&" + criteria + "!=" + 0 + ""
+        lowBRNGErrors = {}
+        subDF = videodata[[selectColumns]]
 
+        lowBRNGErrors = subDF.query(criteriaEquation)
+        if lowBRNGErrors.isEmpty:
+              pass
+        else:
+              lowBRNGErrors.assign(errortype="Low BRNG")
+        return lowBRNGErrors
 
+    standardvalues = setStandardValues(videobitdepth)
+    checkLuma (standardvalues,videodata)
+    checkChromaU (standardvalues,videodata)
+    checkChromaV (standardvalues,videodata)
+    
 
