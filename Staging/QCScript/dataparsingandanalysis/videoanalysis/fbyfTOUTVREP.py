5b812fe24ea2876c694947c1e368cbe4243e583f
SophiaFrancis1 <112432333+SophiaFrancis1@users.noreply.github.com>
2023-11-08 14:52:02 -0600

    Fixes and Updating TOUT VREP

--- Staging/QCScript/dataparsingandanalysis/videoanalysis/fbyfTOUTVREP.py
+++ Staging/QCScript/dataparsingandanalysis/videoanalysis/fbyfTOUTVREP.py
@@ -1,24 +1,57 @@
-def checkErrors (videoBitDepth):
-    def checkTOUT (videoBitDepth):
-        criteria = "TOUT"
-        check(criteria)
+levels = ["ideal", "MAX"]
+criteria = ["TOUT", "VREP"]
+level = 0
+item = 0
+def runCriteria (videoBitDepth, videodata):
+    while item <= criteria:
+        criteriaToCheck = criteria[item]
+        runLevels(videoBitDepth, videodata, criteriaToCheck)
+        item = item + 1
 
-    def check (criteria):
-        criteriavalue = videoBitDepth.get(criteria["ideal"])
-        selectColumns = "" + criteria + "," + "Frame Time" + ""
-        subDF = subDF(selectColumns)
-        idealCriteriaEquation = "" + criteria + "==" + criteriavalue + ""
-        criteriaErrors = {}
-
-        errors = subDF.query(idealCriteriaEquation)
-        if subDF.empty:
-            pass
-        else:
-            if criteria.contains ("ideal"):
-                errortype = "VREP"
-            elif criteria.contains ("")
+def runLevels (videoBitDepth,videodata, criteriaToCheck):
+    while level <= levels:
+        levelToCheck = levels[level]
+        errors = setUpCheckErrors(criteria, levelToCheck, criteriaToCheck, videoBitDepth, videodata)
+        level = level + 1
+        return errors
 
 
-    def subDF (videodata, selectColumns):
-        subDF = videodata[[selectColumns]]
-        return subDF
+def setUpCheckErrors (criteria, levelToCheck, criteriaToCheck, videoBitDepth, videodata):
+    criteriaValue = videoBitDepth.get(criteria[levelToCheck])
+    data = setColumn(videodata,criteriaToCheck)
+    equationSign = setEquationSign(criteriaValue)
+    Equation = setEquation(equationSign, criteria, criteriaValue)
+    errorType = setErrorType(criteriaToCheck)
+    checkErrors(Equation,data, errorType)
+
+def checkErrors(Equation, data, errorType):
+    errors = data.query(Equation)
+    errors = assignETColumn(data,errorType)
+    return errors
+
+
+def setEquation(equationSign, criteriaValue, criteria):
+    Equation = ""+criteria+equationSign+criteriaValue+""
+    return Equation
+   
+def setEquationSign (criteriaValue):
+    #Ideal Value
+    if criteriaValue == 0:
+        equationSign = "=="
+    #Max Value
+    elif criteriaValue > 0:
+        equationSign = ">="
+    return equationSign
+
+def setColumn (criteriaToCheck, videodata):
+    viewColumns = ""+criteriaToCheck+"Frame Time" + "Frame" + ""
+    data = videodata(viewColumns)
+    return data
+
+def setErrorType (criteriaToCheck):
+    errorType = criteriaToCheck
+    return errorType
+
+def assignETColumn (data, errorType):
+    data = data.assign(ErrorType = errorType)
+    return data
