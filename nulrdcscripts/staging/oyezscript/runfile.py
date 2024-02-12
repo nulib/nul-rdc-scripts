003e3a8782d30ffe555e1fa5463e71f72b1f888a
SophiaFrancis1 <112432333+SophiaFrancis1@users.noreply.github.com>
2024-02-12 10:23:41 -0600

    Working Script
    This script now works for both single and multi-file running.

--- nulrdcscripts/staging/oyezscript/runfile.py
+++ nulrdcscripts/staging/oyezscript/runfile.py
@@ -1,23 +1,46 @@
 import os
+import subprocess
 from setupparser import args
 
 ext = [".wav", ".mp3"]
 input_path = os.path.normpath(args.input_path)
-output_path = os.path.normpath(args.output_path)
 isDIRTF = os.path.isdir(args.input_path)
 
 if isDIRTF:
     for files in input_path:
-        filename = os.path.basename(files)
-        filename, file_ext = os.path.splitext(filename)
+        filename, file_ext = os.path.splitext(files)
         if file_ext in ext:
             command = (
                 args.mediainfo_path,
                 " -i ",
                 input_path,
                 " --LogFile=",
-                output_path,
+                filename,
                 ".json",
             )
+            fullfilename = filename + ".json"
+            with open(fullfilename, "w") as f:
+                subprocess.run(command, stdout=f)
         else:
             pass
+else:
+    filename, file_ext = os.path.splitext(input_path)
+    if file_ext == ".wav" or file_ext == ".mp3":
+        command = (
+            args.mediainfo_path,
+            "-i",
+            input_path,
+            "--LogFile=",
+            filename,
+            ".json",
+        )
+        fullfilename = filename + ".json"
+        with open(fullfilename, "w") as f:
+            subprocess.run(command, stdout=f)
+
+    else:
+        raise ValueError("This file is not a .wav or .mp3 file.")
+
+filecheck = filename + ".json"
+with open(filecheck, "r") as file:
+    file.read()
