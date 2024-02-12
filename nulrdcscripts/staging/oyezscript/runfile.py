a97f3918c20881b918642072548b1912cc50d63c
SophiaFrancis1 <112432333+SophiaFrancis1@users.noreply.github.com>
2024-02-12 10:31:34 -0600

    Updated to run with poetry

--- nulrdcscripts/staging/oyezscript/runfile.py
+++ nulrdcscripts/staging/oyezscript/runfile.py
@@ -2,45 +2,51 @@ import os
 import subprocess
 from setupparser import args
 
-ext = [".wav", ".mp3"]
-input_path = os.path.normpath(args.input_path)
-isDIRTF = os.path.isdir(args.input_path)
 
-if isDIRTF:
-    for files in input_path:
-        filename, file_ext = os.path.splitext(files)
-        if file_ext in ext:
+def main():
+    ext = [".wav", ".mp3"]
+    input_path = os.path.normpath(args.input_path)
+    isDIRTF = os.path.isdir(args.input_path)
+
+    if isDIRTF:
+        for files in input_path:
+            filename, file_ext = os.path.splitext(files)
+            if file_ext in ext:
+                command = (
+                    args.mediainfo_path,
+                    " -i ",
+                    input_path,
+                    " --LogFile=",
+                    filename,
+                    ".json",
+                )
+                fullfilename = filename + ".json"
+                with open(fullfilename, "w") as f:
+                    subprocess.run(command, stdout=f)
+            else:
+                pass
+    else:
+        filename, file_ext = os.path.splitext(input_path)
+        if file_ext == ".wav" or file_ext == ".mp3":
             command = (
                 args.mediainfo_path,
-                " -i ",
+                "-i",
                 input_path,
-                " --LogFile=",
+                "--LogFile=",
                 filename,
                 ".json",
             )
             fullfilename = filename + ".json"
             with open(fullfilename, "w") as f:
                 subprocess.run(command, stdout=f)
+
         else:
-            pass
-else:
-    filename, file_ext = os.path.splitext(input_path)
-    if file_ext == ".wav" or file_ext == ".mp3":
-        command = (
-            args.mediainfo_path,
-            "-i",
-            input_path,
-            "--LogFile=",
-            filename,
-            ".json",
-        )
-        fullfilename = filename + ".json"
-        with open(fullfilename, "w") as f:
-            subprocess.run(command, stdout=f)
+            raise ValueError("This file is not a .wav or .mp3 file.")
 
-    else:
-        raise ValueError("This file is not a .wav or .mp3 file.")
+    filecheck = filename + ".json"
+    with open(filecheck, "r") as file:
+        file.read()
 
-filecheck = filename + ".json"
-with open(filecheck, "r") as file:
-    file.read()
+
+if __name__ == "__main__":
+    main()
