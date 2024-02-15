6cb155e56ffd9dbe101af7e08d9867b3a9e9576e
SophiaFrancis1 <112432333+SophiaFrancis1@users.noreply.github.com>
2024-02-15 09:21:38 -0600

    Increasedfontsize

--- nulrdcscripts/tools/ffplaywindow/main.py
+++ nulrdcscripts/tools/ffplaywindow/main.py
@@ -16,7 +16,7 @@ def main():
         font = fontfile_mac
 
     ffplay_filter = (
-        "-vf split=5[a][b][c][d][e];[a]copy,drawtext=text='%{pts\:hms}':box=1:boxborderw=5:x=(w-text_w)/2:y=h-(text_h*2):fontsize=16:fontfile="
+        "-vf split=5[a][b][c][d][e];[a]copy,drawtext=text='%{pts\:hms}':box=1:boxborderw=5:x=(w-text_w)/2:y=h-(text_h*2):fontsize=20:fontfile="
         + font
         + "[a1];[b]field=top,format=yuv422p,waveform=scale=digital:intensity=0.1:mode=column:mirror=1:c=1:f=lowpass:e=instant:graticule=green:flags=numbers+dots[b1];[c]field=bottom,format=yuv422p,waveform=scale=digital:intensity=0.1:mode=column:mirror=1:c=1:f=lowpass:e=instant:graticule=green:flags=numbers+dots[c1];[d]format=yuv422p,vectorscope=i=0.04:mode=color2:c=1:envelope=instant:graticule=green:flags=name,scale=512:512,drawbox=w=9:h=9:t=1:x=128-3:y=512-452-5:c=sienna@0.8,drawbox=w=9:h=9:t=1:x=160-3:y=512-404-5:c=sienna@0.8,drawbox=w=9:h=9:t=1:x=192-3:y=512-354-5:c=sienna@0.8,drawbox=w=9:h=9:t=1:x=224-3:y=512-304-5:c=sienna@0.8,drawgrid=w=32:h=32:t=1:c=white@0.1,drawgrid=w=256:h=256:t=1:c=white@0.2[d1];[e]scale=512:ih,signalstats='out=brng:color="
         + args.highlight_color
