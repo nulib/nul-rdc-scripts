af8a0ecb039f687946b6c6262f9c77f3f5f23be2
alec-bertoy <alec.bertoy@northwestern.edu>
2024-03-05 14:44:36 -0600

    json file output and first attempt at finding silence

--- nulrdcscripts/aqc/aqc.py
+++ nulrdcscripts/aqc/aqc.py
@@ -7,6 +7,7 @@ from datetime import timedelta
 import matplotlib.pyplot as plt
 import pandas as pd
 import re
+import json
 from operator import itemgetter
 from itertools import *
 from nulrdcscripts.aqc.parser import args
@@ -17,6 +18,8 @@ if sys.version_info[0] < 3:
 ASETNSAMPLES = 96000
 FLAT_FACTOR_THRESH = 15
 COOLDOWN = 5
+SILENCE_MIN_LENGTH = 5
+ENTROPY_THRESH = .5
 
 def main():
 
@@ -30,16 +33,23 @@ def main():
         quit()
 
     infile = os.path.normpath(args.infile)
-    outfile = os.path.splitext(infile)[0] + ".txt"
+    txtfile = os.path.splitext(infile)[0] + ".txt"
+    jsonfile = os.path.splitext(infile)[0] + ".json"
 
-    adf = get_astats(infile, outfile)
+    jsondata = {}
+
+    adf = get_astats(infile, txtfile)
     lstats = get_lstats(infile)
 
     # create txt with astats data and load txt into dataframe
-    print_lstats(lstats)
-    find_clipping(adf)
+    jsondata.update(print_lstats(lstats))
+    jsondata.update(find_clipping(adf))
+    jsondata.update(find_silence(adf))
 
-    graph_astats(adf)
+    with open(jsonfile, "w", encoding='utf-8') as f:
+        json.dump(jsondata, f, ensure_ascii=False, indent=4)
+
+    # graph_astats(adf)
 
 def get_astats(infile, outfile):
     
@@ -128,6 +138,16 @@ def print_lstats(lstats):
     print(f"LUFS-I:\t{lstats["input_i"]:9.2f} LUFS")
     print(f"LRA:\t{lstats["input_lra"]:9.2f} LU\n")
 
+    loudness_dict = {
+        "Loudness": {
+            "Max TP":lstats["input_tp"],
+            "LUFS-I":lstats["input_i"],
+            "LRA":lstats["input_lra"],
+        }
+    }
+
+    return loudness_dict
+
 def find_clipping(adf):
     # find frames with clipping (flat factor being greater than a given threshold)
     flats = adf.index[adf['Overall.Flat_factor'] > FLAT_FACTOR_THRESH].tolist()
@@ -141,13 +161,32 @@ def find_clipping(adf):
         else:
             source_clips.append(flat)
 
-    print_warnings(digi_clips, "Potential digital clipping", adf)
-    print_warnings(source_clips, "Potential source clipping", adf)
-    # print("WARNING: potential clipping")
-    # print("---------------------------")
+    clipping_data = {}
+    digi_clip_data = print_warnings(digi_clips, "Potential digital clipping", adf)
+    if(digi_clip_data):
+        clipping_data.update(digi_clip_data)
+    source_clip_data = print_warnings(source_clips, "Potential source clipping", adf)
+    if(source_clip_data):
+        clipping_data.update(source_clip_data)
+
+    return {
+        "Clipping": clipping_data
+    }
+
+def find_silence(adf):
+    silences = adf.index[adf['Overall.Entropy'] < ENTROPY_THRESH].tolist()
+
+    silent_data = print_warnings(silences, "Potential silence", adf, SILENCE_MIN_LENGTH)
+
+    return {
+        "Silence": silent_data
+    }
     
 
-def print_warnings(timestamps, warning, adf):
+def print_warnings(timestamps, warning, adf, min_length=0):
+
+    warnings = {}
+
 
     # create groups of consecutive frames
     groups = []
@@ -156,6 +195,13 @@ def print_warnings(timestamps, warning, adf):
 
     if not groups:
         return
+    
+    long_groups = []
+    for group in groups:
+        if len(group) >=  min_length:
+            long_groups.append(group)
+    groups = long_groups
+
     for group in groups:
         start_sec = adf['pts_time'][group[0]]
         end_sec = adf['pts_time'][group[-1]]
@@ -166,18 +212,20 @@ def print_warnings(timestamps, warning, adf):
         # for short durations, treat the clipping as a single event
         if end_sec - start_sec < COOLDOWN:
             print(warning + ": " + start_time)
+            warnings.update({start_time: warning})
         else:
             print(warning + ": " + start_time + " to " + end_time)
+            warnings.update({(start_time + " - " + end_time): warning})
+
     print()
+    return warnings
 
 def graph_astats(adf):
     columns = [
-        "Overall.Peak_level", 
         "Overall.RMS_level", 
-        "Overall.Flat_factor", 
         "Overall.Entropy",  
     ]
-    adf.plot(x='pts_time',y=columns, subplots=True, layout=(2,2))
+    adf.plot(x='pts_time',y=columns, subplots=True, layout=(2,1))
     plt.show()
 
 def df_print(df):
