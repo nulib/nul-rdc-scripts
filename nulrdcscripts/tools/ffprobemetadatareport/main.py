21107552fc01b450c35956551ff5c79fed04686d
SophiaFrancis1 <112432333+SophiaFrancis1@users.noreply.github.com>
2024-04-24 11:52:17 -0500

    updates

--- nulrdcscripts/tools/ffprobemetadatareport/main.py
+++ nulrdcscripts/tools/ffprobemetadatareport/main.py
@@ -2,64 +2,224 @@ import os
 import subprocess
 import json
 from nulrdcscripts.tools.ffprobemetadatareport.params import args
-def setandcheckFFProbePath ():
+
+
+def setandcheckFFProbePath():
     if args.ffprobe_path:
         ffprobe_path = os.path.abspath(args.ffprobe_path)
     else:
         ffprobe_path = "ffprobe"
-    
+
     try:
         subprocess(ffprobe_path, "-version")
         return ffprobe_path
     except:
-        raise Exception ("Cannot find FFProbe Path")
+        raise Exception("Cannot find FFProbe Path")
+
+
 def generatereport(filename, input_path, ffprobe_path):
-    videoExt = [".mp4",".mkv",".mov"]
-    file,ext = os.path.splitext(input_path)
+    videoExt = [".mp4", ".mkv", ".mov"]
+    file, ext = os.path.splitext(input_path)
     if ext in videoExt:
-        video_output= json.loads(subprocess.check_output([ffprobe_path, "-v","error","-select_streams","v","-show_entries," "stream=codec_name,avg_frame_rate,codec_time_base,width,height,pix_fmt,sample_aspect_ratio,display_aspect_ratio,color_range,color_space,color_transfer,color_primaries,chroma_location,field_order,codec_tag_string", input_path,"-of","json",]).decode("ascii").rstrip()
+        video_output = json.loads(
+            subprocess.check_output(
+                [
+                    ffprobe_path,
+                    "-v",
+                    "error",
+                    "-select_streams",
+                    "v",
+                    "-show_entries,"
+                    "stream=codec_name,avg_frame_rate,codec_time_base,width,height,pix_fmt,sample_aspect_ratio,display_aspect_ratio,color_range,color_space,color_transfer,color_primaries,chroma_location,field_order,codec_tag_string",
+                    input_path,
+                    "-of",
+                    "json",
+                ]
             )
-        video_codec_name_list = [stream.get("codec_name") for stream in (video_output["streams"])]
+            .decode("ascii")
+            .rstrip()
+        )
+        video_codec_name_list = [
+            stream.get("codec_name") for stream in (video_output["streams"])
+        ]
         width = [stream.get("width") for stream in (video_output["streams"])][0]
         height = [stream.get("height") for stream in (video_output["streams"])][0]
-        pixel_format = [stream.get("pix_fmt") for stream in (video_output["streams"])][0]
-        sar = [stream.get("sample_aspect_ratio") for stream in (video_output["streams"])][0]
-        dar = [stream.get("display_aspect_ratio") for stream in (video_output["streams"])][0]
-        frame_rate = [stream.get("avg_frame_rate") for stream in (video_output["streams"])][0]
-        color_space = [stream.get("color_space") for stream in (video_output["streams"])][0]
-        color_range = [stream.get("color_range") for stream in (video_output["streams"])][0]
-        color_transfer = [stream.get("color_transfer") for stream in (video_output["streams"])][0]
-        color_primaries = [stream.get("color_primaries") for stream in (video_output["streams"])][0]
-    else: 
+        pixel_format = [stream.get("pix_fmt") for stream in (video_output["streams"])][
+            0
+        ]
+        sar = [
+            stream.get("sample_aspect_ratio") for stream in (video_output["streams"])
+        ][0]
+        dar = [
+            stream.get("display_aspect_ratio") for stream in (video_output["streams"])
+        ][0]
+        frame_rate = [
+            stream.get("avg_frame_rate") for stream in (video_output["streams"])
+        ][0]
+        color_space = [
+            stream.get("color_space") for stream in (video_output["streams"])
+        ][0]
+        color_range = [
+            stream.get("color_range") for stream in (video_output["streams"])
+        ][0]
+        color_transfer = [
+            stream.get("color_transfer") for stream in (video_output["streams"])
+        ][0]
+        color_primaries = [
+            stream.get("color_primaries") for stream in (video_output["streams"])
+        ][0]
+    else:
         pass
-    audio_output = json.loads(subprocess.check_output([ffprobe_path, "-v","error","-select_streams","a","-show_entries","stream=codec_long_name,bits_per_raw_sample,sample_rate,channels", input_path,"-of","json",]).decode("ascii").rstrip())
-    format_output = json.loads(subprocess.check_output([ffprobe_path,"-v","error","-show_entries","format=duration,size,nb_streams",input_path,"-of","json",]).decode("ascii").rstrip())
-    data_output = json.loads(subprocess.check_output(["ffprobe_path","-v","error","-select_stream","d","-show_entries","stream=codec_tag_string",input_path, "-of","json"]).decode("ascii").rstrip())
-    attachment_output = json.loads(subprocess.check_output([ffprobe_path, "-v","error","-select_streams","t","-show_entries","stream_tags=filename",input_path,"-of","json"]).decode("ascii").rstrip())
+    audio_output = json.loads(
+        subprocess.check_output(
+            [
+                ffprobe_path,
+                "-v",
+                "error",
+                "-select_streams",
+                "a",
+                "-show_entries",
+                "stream=codec_long_name,bits_per_raw_sample,sample_rate,channels",
+                input_path,
+                "-of",
+                "json",
+            ]
+        )
+        .decode("ascii")
+        .rstrip()
+    )
+    format_output = json.loads(
+        subprocess.check_output(
+            [
+                ffprobe_path,
+                "-v",
+                "error",
+                "-show_entries",
+                "format=duration,size,nb_streams",
+                input_path,
+                "-of",
+                "json",
+            ]
+        )
+        .decode("ascii")
+        .rstrip()
+    )
+    data_output = json.loads(
+        subprocess.check_output(
+            [
+                "ffprobe_path",
+                "-v",
+                "error",
+                "-select_stream",
+                "d",
+                "-show_entries",
+                "stream=codec_tag_string",
+                input_path,
+                "-of",
+                "json",
+            ]
+        )
+        .decode("ascii")
+        .rstrip()
+    )
+    attachment_output = json.loads(
+        subprocess.check_output(
+            [
+                ffprobe_path,
+                "-v",
+                "error",
+                "-select_streams",
+                "t",
+                "-show_entries",
+                "stream_tags=filename",
+                input_path,
+                "-of",
+                "json",
+            ]
+        )
+        .decode("ascii")
+        .rstrip()
+    )
 
     tags = [streams.get("tags") for streams in (attachment_output["streams"])]
     attachment_list = []
     for i in tags:
         attachmentFilename = [i.get("filename")]
         attachment_list.extend(attachmentFilename)
-    audio_codec_name_list = [stream.get("codec_long_name") for stream in (audio_output["streams"])]
-    data_streams = [stream.get("codec_tag_string") for stream in (data_output["streams"])]
-    audio_bitrate = [stream.get("bits_per_raw_sample") for stream in (audio_output["streams"])][0]
-    audio_sample_rate = [stream.get("sample_rate") for stream in (audio_output["streams"])][0]
+    audio_codec_name_list = [
+        stream.get("codec_long_name") for stream in (audio_output["streams"])
+    ]
+    data_streams = [
+        stream.get("codec_tag_string") for stream in (data_output["streams"])
+    ]
+    audio_bitrate = [
+        stream.get("bits_per_raw_sample") for stream in (audio_output["streams"])
+    ][0]
+    audio_sample_rate = [
+        stream.get("sample_rate") for stream in (audio_output["streams"])
+    ][0]
     audio_channels = [stream.get("channels") for stream in (audio_output["streams"])]
     audio_stream_count = len(audio_codec_name_list)
+    if ext in videoExt:
+        file_metadata = {
+            "filename": filename,
+            "file size": format_output.get("format")["size"],
+            "duration": format_output.get("format")["duration"],
+            "streams": format_output.get("format")["nb_streams"],
+            "video streams": video_codec_name_list,
+            "audio streams": audio_codec_name_list,
+            "data streams": data_streams,
+            "attachments": attachment_list,
+        }
+        techMetaV = {
+            "width": width,
+            "height": height,
+            "sample_aspect_ratio": sar,
+            "display_aspect_ratio": dar,
+            "pixel format": pixel_format,
+            "framerate": frame_rate,
+            "color space": color_space,
+            "color_range": color_range,
+            "color_primaries": color_primaries,
+            "color_transfer": color_transfer,
+        }
+        techMetaA = {
+            "audio stream count": audio_stream_count,
+            "audio bitrate": audio_bitrate,
+            "audio sample rate": audio_sample_rate,
+            "channels": audio_channels,
+        }
+        ffprobe_metadata = {
+            "file metadata": file_metadata,
+            "techMetaV": techMetaV,
+            "techMetaA": techMetaA,
+        }
+    else:
+        file_metadata = {
+            "file size": format_output.get("format")["size"],
+            "duration": format_output.get("format")["duration"],
+            "streams": format_output.get("format")["nb_streams"],
+            "audio streams": audio_codec_name_list,
+            "data streams": data_streams,
+            "attachments": attachment_list,
+        }
+        techMetaA = {
+            "audio stream count": audio_stream_count,
+            "audio bitrate": audio_bitrate,
+            "audio sample rate": audio_sample_rate,
+            "channels": audio_channels,
+        }
+        ffprobe_metadata = {
+            "file metadata": file_metadata,
+            "techMetaA": techMetaA,
+        }
 
-    file_metadata = {
-        "filename":filename, 
-        "file size": format_output.get("format")["size"],
-        "duration":format_output.get("format")["duration"],
-        "streams": format_output.get("format")["nb_streams"],
-        
-    }
+    return ffprobe_metadata
 
-def solorun ():
+
+def solorun():
     input_abs_path = os.path.abspath(args.input_path)
     ffprobe_path = setandcheckFFProbePath()
     ffprobe_abs_path = os.path.abspath(ffprobe_path)
     baseFile = os.path.basename(args.input_path)
-    generatereport(baseFile,input_abs_path,ffprobe_abs_path)
+    generatereport(baseFile, input_abs_path, ffprobe_abs_path)
--- nulrdcscripts/tools/ffprobemetadatareport/params.py
+++ nulrdcscripts/tools/ffprobemetadatareport/params.py
@@ -1,1 +1,35 @@
 import argparse
+
+parser = argparse.ArgumentParser(
+    description="Allows for input to generate the ffprobe video data"
+)
+
+parser.add_argument(
+    "--input",
+    "-i",
+    action="store",
+    dest="input_path",
+    required=True,
+    type=str,
+    help="Enter full path to the file you want to generate metadata for",
+)
+
+parser.add_argument(
+    "--output",
+    "-o",
+    action="store",
+    dest="output_path",
+    type=str,
+    help="Enter the path to where you want your output",
+)
+
+parser.add_argument(
+    "--ffprobe",
+    action="store",
+    dest="ffprobe_path",
+    default="ffprobe",
+    type=str,
+    help="Enter the path to ffprobe if it is not on your path",
+)
+
+args = parser.parse_args()
