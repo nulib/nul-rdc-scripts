bb42edfd7155c0bb7344b6e268d79fdf583af43f
SophiaFrancis1 <112432333+SophiaFrancis1@users.noreply.github.com>
2024-05-15 15:20:52 -0500

    updates

---
+++ nulrdcscripts/tools/ffprobedata/try.py
@@ -0,0 +1,34 @@
+from params import args
+import os
+import subprocess
+
+
+def swapSlashes(item):
+    fixed_path = item.replace("\\", "/")
+    fixed_path = fixed_path.replace(":", "\\\\:")
+    return fixed_path
+
+
+def singlevideo(norm_input):
+    fixed_input = swapSlashes(norm_input)
+    filepath = norm_input.replace(".mp4", "_framebyframe.xml")
+    command = [
+        args.ffprobe_path,
+        "-f",
+        "lavfi",
+        "movie=" + fixed_input + ",signalstats='stat=tout+vrep+brng'",
+        "-show_frames",
+        "-of",
+        "json",
+    ]
+    with open(filepath, "w") as f:
+        subprocess.run(command, stdout=f)
+        f.close()
+
+
+def main():
+    norm_input = os.path.abspath(args.input_path)
+    singlevideo(norm_input)
+
+
+main()
