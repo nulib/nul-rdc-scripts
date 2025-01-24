d3248ba907cd372b95fcfc9afa6f1be7722554a2
SophiaFrancis1 <112432333+SophiaFrancis1@users.noreply.github.com>
2025-01-24 09:55:38 -0600

    updated for deck config

--- nulrdcscripts/vproc/corefuncs.py
+++ nulrdcscripts/vproc/corefuncs.py
@@ -46,32 +46,6 @@ def output_check(indir):
     return outdir
 
 
-def hashlib_md5(filename):
-    """
-    Uses hashlib to return an MD5 checksum of an input filename
-    Credit: IFI scripts
-    """
-    read_size = 0
-    last_percent_done = 0
-    chksm = hashlib.md5()
-    total_size = os.path.getsize(filename)
-    with open(filename, "rb") as f:
-        while True:
-            # 2**20 is for reading the file in 1 MiB chunks
-            buf = f.read(2**20)
-            if not buf:
-                break
-            read_size += len(buf)
-            chksm.update(buf)
-            percent_done = 100 * read_size / total_size
-            if percent_done > last_percent_done:
-                sys.stdout.write("[%d%%]\r" % percent_done)
-                sys.stdout.flush()
-                last_percent_done = percent_done
-    md5_output = chksm.hexdigest()
-    return md5_output
-
-
 def mediaconch_policy_exists(policy_path):
     """
     checks that the specified mediaconch policy exists
