4ec46a9906691fbf660919cd8bf8da0bde667973
SophiaFrancis1 <112432333+SophiaFrancis1@users.noreply.github.com>
2025-02-24 10:26:48 -0600

    Works

--- nulrdcscripts/tools/ffplaywindow/main.py
+++ nulrdcscripts/tools/ffplaywindow/main.py
@@ -1,6 +1,5 @@
 import os
 import subprocess
-import tempfile
 from nulrdcscripts.tools.ffplaywindow.scriptparser import args
 
 
@@ -24,25 +23,28 @@ def main():
         font = "/Windows/Fonts/arial.ttf"
         ffplayfilter = declareFfplayfilter(font)
         command = ffplay_path + " " + "-i" + " " + input_path + " " + ffplayfilter
-        result = subprocess.run(command)
-        print("Command output:", result.stdout)
-        print("Command error:", result.stderr)
     else:
         font = "/Library/Fonts/Arial.ttf"  # Specify a valid font path for macOS
         ffplayfilter = declareFfplayfilter(font)
-        command = ffplay_path + " " + "-i" + " " + input_path + " " + ffplayfilter
+        command = [
+            'ffplay', input_path,
+            '-vf', (
+                'split=5[a][b][c][d][e];'
+                '[a]copy,drawtext=text=\'%{pts\\:hms}\':box=1:boxborderw=5:x=(w-text_w)/2:y=h-(text_h*2):fontsize=20:fontfile=font[a1];'
+                '[b]field=top,format=yuv422p,waveform=scale=digital:intensity=0.1:mode=column:mirror=1:c=1:f=lowpass:e=instant:graticule=green:flags=numbers+dots[b1];'
+                '[c]field=bottom,format=yuv422p,waveform=scale=digital:intensity=0.1:mode=column:mirror=1:c=1:f=lowpass:e=instant:graticule=green:flags=numbers+dots[c1];'
+                '[d]format=yuv422p,vectorscope=i=0.04:mode=color2:c=1:envelope=instant:graticule=green:flags=name,scale=512:512,'
+                'drawbox=w=9:h=9:t=1:x=128-3:y=512-452-5:c=sienna@0.8,drawbox=w=9:h=9:t=1:x=160-3:y=512-404-5:c=sienna@0.8,'
+                'drawbox=w=9:h=9:t=1:x=192-3:y=512-354-5:c=sienna@0.8,drawbox=w=9:h=9:t=1:x=224-3:y=512-304-5:c=sienna@0.8,'
+                'drawgrid=w=32:h=32:t=1:c=white@0.1,drawgrid=w=256:h=256:t=1:c=white@0.2[d1];'
+                '[e]scale=512:ih,signalstats=out=brng:color=red[e1];'
+                '[a1][b1][c1][e1][d1]xstack=inputs=5:layout=0_0|0_h0|0_h0+h1|w0_0|w0_h0'
+            ),
+            '-af', 'channelmap=0|1:stereo'
+]
 
-        try:
-            with tempfile.NamedTemporaryFile(mode="w", delete=False) as temp_file:
-                temp_file.write("#!/bin/sh\n" + command)
-                temp_file_path = temp_file.name
-            os.chmod(temp_file_path, 0o755)  # Make the temp file executable
-            print(f"Executing command from temp file: {temp_file_path}")
-            result = subprocess.run([temp_file_path], capture_output=True, text=True)
-            print("Command output:", result.stdout)
-            print("Command error:", result.stderr)
-        except Exception as e:
-            print(f"An error occurred: {e}")
+# Run the ffplay command
+    subprocess.run(command)
 
     print(
         "To exit the playback window, while in window use the 'esc' key. To fast-forward or rewind, use the respective arrow keys."
